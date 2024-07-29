235827b3add699e8c1e79faf82c7e20fc0d3430e
Fobious <Fabricio_tora@hotmail.com>
2024-07-29 23:37:27 +0000

    Adicionado código para juntar strings

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+
+# Printar algo no console
 print("Hello World!")
+# Printar texto em mais de uma linha
+print('Fabrício')
+print('Dias')
+print("Cocada\nDoce")
+print("""Fabrício
+Dias""")
+
 print("Meu Nome é Fabrício Dias")
+# Para juntar duas Strings. Podemos usar o +
+string0 = "Alunos"
+string1 = "Infnet"
+
+print(string0 + " "+ string1)
